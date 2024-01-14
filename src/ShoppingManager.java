a73320658c683bfe1d05de426409933a4a815b8b
HasalaAbhilasha <hasala200301abhilasha@gmail.com>
2024-01-14 06:23:00 +0530

    Delete

--- src/ShoppingManager.java
+++
@@ -1,7 +0,0 @@
-public interface ShoppingManager {
-    void addProduct(); // Method to add a new product
-    void deleteProduct(); // Method to delete a product
-    void displayProducts(); // Method to display all products
-    void saveProducts(); // Method to save products to a file
-    void loadProducts(); // Method to load products from a file
-}
